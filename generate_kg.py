d37f677e0fe4a5ef072004f70091db36ca9aa89f
Claire Gillaspie <gillaspieclaire@gmail.com>
2025-08-05 16:15:46 -0400

    cleaning up kg code

--- generate_kg.py
+++ generate_kg.py
@@ -1,11 +1,8 @@
 import os
-import re
 from dotenv import load_dotenv
 import pickle
 import json
 from collections import defaultdict
-from typing import Any
-from pydantic import BaseModel
 from langchain_core.documents import Document
 from langchain_neo4j import Neo4jGraph
 from langchain_community.graphs.graph_document import GraphDocument, Node, Relationship
@@ -21,51 +18,45 @@ psw = os.environ['PASSWORD_DD']
 graph = Neo4jGraph(url=uri, username=usr, password=psw)
 
 
-class Element(BaseModel):
-    type: str
-    text: Any
-
-
 # processing a single JSON into a GraphDocument
 def process_json_response(json_data, filename):
-    print(f"Processing file: {filename}")
-    
     # extract relations from JSON
-    relations = json_data.get("relations", [])
+    relations = json_data.get('relations', [])
 
+    ### do we want to deduplicate by document or globally??
     nodes_set = set()
     relationships = []
 
     for rel in relations:
         try:
             # add nodes to set for deduplication
-            nodes_set.add((rel["head"], rel["head_type"]))
-            nodes_set.add((rel["tail"], rel["tail_type"]))
+            nodes_set.add((rel['head'], rel['head_type']))
+            nodes_set.add((rel['tail'], rel['tail_type']))
 
             # add types to list for normalizing
             varying_capitals((rel['head_type'], rel['tail_type']))
 
             # create source and target entity nodes
             source_node = Node(
-                id=rel["head"],
-                type=rel["head_type"]
+                id=rel['head'],
+                type=rel['head_type']
             )
             target_node = Node(
-                id=rel["tail"],
-                type=rel["tail_type"]
+                id=rel['tail'],
+                type=rel['tail_type']
             )
             
-            # create relationship
+            # create relationship between source & target
             relationships.append(
                 Relationship(
                     source=source_node,
                     target=target_node,
-                    type=rel["relation"]
+                    type=rel['relation']
                 )
             )
 
         except Exception as e:
-            print(f"Error processing relation in {filename}: {rel}, Error: {e}")
+            print(f'Error processing relation: {rel}, Error: {e}')
 
     # build list of nodes
     nodes = [Node(id=el[0], type=el[1]) for el in list(nodes_set)]
@@ -74,38 +65,41 @@ def process_json_response(json_data, filename):
     source_content = f"Title: {json_data.get('title', 'Unknown')}\nAbstract: {json_data.get('abstract', 'Unknown')}"
     source_doc = Document(
         page_content=source_content,
-        metadata={"filename": filename}
+        metadata={'filename': filename}
     )
     
     # return constructed GraphDocument
     return nodes, relationships, source_doc
 
 
-# append all types to list
+# map all capitalization variants using lowercase as key
 different_capitals = defaultdict(set)
 def varying_capitals(types):
     for type in types:
-        normalized = type.lower()
-        different_capitals[normalized].add(type)
+        lowercase = type.lower()
+        different_capitals[lowercase].add(type)
 
 # count number of uppercase letters
 def count_uppercase(type):
     return len([letter for letter in type if letter.isupper()])
 
-# map types of different capitalization to max capitalized version
-mapped_types = defaultdict(str)
-def map_capitalization_variants(different_capitals):    
-    for _, value_set in different_capitals.items():
-        value_list = list(value_set)
+# select most capitalized version of each type
+def map_max_capitalized(different_capitals):    
+    mapped_types = defaultdict(str)
+
+    for _, type_set in different_capitals.items():
+        type_list = list(type_set)
         # sort by number of uppercase characters descending
-        if len(value_list) > 1:
-            value_list.sort(key=count_uppercase, reverse=True)
+        if len(type_list) > 1:
+            type_list.sort(key=count_uppercase, reverse=True)
         # use lowercase type as key, and most uppercase version the value
-        mapped_types[value_list[0].lower()] = (value_list[0])
+        mapped_types[type_list[0].lower()] = (type_list[0])
+
+    return mapped_types
 
 
 # create final GraphDocument
-def create_normalized_graph_documents(nodes, relationships, source_doc):
+def create_normalized_graph_documents(nodes, relationships, source_doc, mapped_types):
     for node in nodes:
         node.type = mapped_types.get(node.type.lower())
     
@@ -117,29 +111,31 @@ def create_normalized_graph_documents(nodes, relationships, source_doc):
 
 
 # process all JSONs in given folder and create Neo4j graph
-def process_json_folder(folder_path: str = "platinum_relations") -> None:
+def process_json_folder(folder_path):
     # check if folder exists
     if not os.path.exists(folder_path):
-        print(f"{folder_path} does not exist")
+        print(f'{folder_path} does not exist')
         return
     
     # extract all JSONs
     json_files = [f for f in os.listdir(folder_path) if f.endswith('.json')]
     
     # check for cached results
-    pickle_file = "json_graph_output.pkl"
+    pickle_file = 'json_graph_output.pkl'
     if os.path.exists(pickle_file):
-        print("Found cached results")
-        with open(pickle_file, "rb") as f:
+        print('Processing cached results')
+        with open(pickle_file, 'rb') as f:
             graph_documents = pickle.load(f)
     else:
-        print("Processing JSON files")
+        print('Processing JSON files')
         document_info = []
         
         for filename in json_files:
             file_path = os.path.join(folder_path, filename)
             
             try:
+                print(f'Processing file: {filename}')
+
                 # load JSON
                 with open(file_path, 'r', encoding='utf-8') as f:
                     json_data = json.load(f)
@@ -149,34 +145,34 @@ def process_json_folder(folder_path: str = "platinum_relations") -> None:
                 document_info.append([nodes, relationships, source_doc])
                 
             except Exception as e:
-                print(f"Error processing {filename}: {e}")
+                print(f'Error processing {filename}: {e}')
                 continue
 
     # map all differing capitalizations to the most capitalized version
-    map_capitalization_variants(different_capitals)
+    mapped_types = map_max_capitalized(different_capitals)
 
     # build normalized GraphDocuments
     graph_documents = []
     for document in document_info:
         graph_documents.append(
-            create_normalized_graph_documents(document[0], document[1], document[2])
+            create_normalized_graph_documents(document[0], document[1], document[2], mapped_types)
         )
 
     # cache the results for future re-use
-    with open(pickle_file, "wb") as f:
+    with open(pickle_file, 'wb') as f:
         pickle.dump(graph_documents, f)
 
     # clear graph if already populated
     graph.query("MATCH (n) DETACH DELETE n")
     
-    # add GraphDocument to Neo4j graph
+    # add GraphDocuments to Neo4j graph
     graph.add_graph_documents(
         graph_documents,
         baseEntityLabel=True, 
         include_source=True
     )
     
-    print(f"Complete")
+    print('\n\nComplete')
 
-if __name__ == "__main__":
-    process_json_folder("platinum_relations")
+if __name__ == '__main__':
+    process_json_folder('platinum_relations')
